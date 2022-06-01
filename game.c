fdcd45930fcf3e40ee904bb59f89bc42e3a4cdc8
BhaT <85089251+BhaT844@users.noreply.github.com>
2022-06-01 23:07:59 +0900

    Update game.c

--- game.c
+++ game.c
@@ -5,13 +5,12 @@
 
 int** animalArray;
 int** checkAnimal;
-char** strAnimal;
-char* strDifficulty;
-int difficulty;
-short height = 3;
-short width = 4;
+const char** strAnimal;
+const char* strDifficulty;
+short height;
+short width;
 
-void initDifficulty();
+void initDifficulty(int diff);
 void initAnimalArray();
 void initAnimalName();
 void shuffleAnimal();
@@ -25,13 +24,30 @@ int foundAllAnimals();
 int main(void)
 {
 	srand(time(NULL));
+	short difficulty;
+	short repeat = 1;
 
-	initDifficulty();
+	while (repeat--)
+	{
+		printf("(1: 쉬움    2: 보통    3: 어려움)\n");
+		printf("난이도를 선택하세요  >>   ");
+		scanf_s("%d", &difficulty); 
+		if (0 > difficulty || difficulty > 3)
+		{
+			printf("올바른 난이도가 아닙니다. 다시 선택하십시오\n\n");
+			repeat++;
+		}
+	}
+
+	initDifficulty(difficulty);
 	initAnimalArray();
 	initAnimalName();
-
 	shuffleAnimal();
-	
+
+	system("cls");
+
+	printf("%s 난이도\n\n\n\n", strDifficulty);
+
 	int failCount = 0;
 
 	while (1)
@@ -39,45 +55,45 @@ int main(void)
 		int select1 = 0;
 		int select2 = 0;
 
-		printf("%s 난이도\n", strDifficulty);
-		printAnimals();  //정답표시모드
+		//printAnimals();  //정답표시모드
 		printQuestion();
-		printf("뒤집을 카드를 2개 고르세요  >>  ");
+		printf("\n\n뒤집을 카드를 2개 고르세요  >>  ");
 		scanf_s("%d %d", &select1, &select2);
 
-		if ((select1 == select2) ||
-			(1 > select1 || select1 > height * width) ||
-			(1 > select2 || select2 > height * width))
+		system("cls");
+
+		if ((select1 < 1 || select1 > height * width) || (select2 < 1 || select2 > height * width))
 		{
-			printf("잘못된 값을 입력하셨습니다\n\n");
+			printf("올바르지 않은 수 입니다: %d, %d\n", select1, select2);
+			continue;
+		}
+		else if (select1 == select2)
+		{
+			printf("같은 수 2개는 고를 수 없습니다\n");
 			continue;
 		}
 
-		select1--;
-		select2--;
+		int firstSelect_x = conv_pos_x(select1 - 1);
+		int firstSelect_y = conv_pos_y(select1 - 1);
 
-		int firstSelect_x = conv_pos_x(select1);
-		int firstSelect_y = conv_pos_y(select1);
-
-		int secondSelect_x = conv_pos_x(select2);
-		int secondSelect_y = conv_pos_y(select2);
+		int secondSelect_x = conv_pos_x(select2 - 1);
+		int secondSelect_y = conv_pos_y(select2 - 1);
 
 		if (checkAnimal[firstSelect_x][firstSelect_y] == 0 &&
-			checkAnimal[secondSelect_x][secondSelect_y] == 0
-			&&
+			checkAnimal[secondSelect_x][secondSelect_y] == 0 &&
 			animalArray[firstSelect_x][firstSelect_y] ==
 			animalArray[secondSelect_x][secondSelect_y])
 		{
-			printf("\n\n정답! : %s발견\n\n", strAnimal[animalArray[firstSelect_x][firstSelect_y]]);
+			printf("정답!\n");
+			printf("%s 발견\n\n\n", strAnimal[animalArray[firstSelect_x][firstSelect_y]]);
 			checkAnimal[firstSelect_x][firstSelect_y] = 1;
 			checkAnimal[secondSelect_x][secondSelect_y] = 1;
 		}
 		else
 		{
-			printf("\n\n 틀렸습니다!\n");
-			printf("%d : %s\n", select1 + 1, strAnimal[animalArray[firstSelect_x][firstSelect_y]]);
-			printf("%d : %s\n", select2 + 1, strAnimal[animalArray[secondSelect_x][secondSelect_y]]);
-			printf("\n\n");
+			printf("틀렸습니다!\n");
+			printf("%d : %s\n", select1, strAnimal[animalArray[firstSelect_x][firstSelect_y]]);
+			printf("%d : %s\n\n", select2, strAnimal[animalArray[secondSelect_x][secondSelect_y]]);
 
 			failCount++;
 		}
@@ -89,50 +105,31 @@ int main(void)
 			break;
 		}
 	}
-	for (int i = 0; i < width; i++)
-	{
-		free(animalArray[i]);
-		free(checkAnimal[i]);
-	}
-	free(animalArray);
-	free(checkAnimal);
-	free(strAnimal);
 	return 0;
 }
 
-void initDifficulty()
+void initDifficulty(int diff)
 {
-	printf("(1: 쉬움    2: 보통    3: 어려움)\n");
-	printf("난이도를 선택하세요  >>   ");
-
-	short i = 1;
-	while (i--)
+	if (diff == 0)
 	{
-		scanf_s("%d", &difficulty);
-		if (0 > difficulty || difficulty > 3)
-		{
-			printf("올바른 난이도가 아닙니다. 다시 선택하십시오  >>  ");
-			i++;
-		}
-	}
-
-	if (difficulty == 0)
-	{
-		strDifficulty = "인세인";
+		strDifficulty = "INSANE";
 		exit(0);
 		height = 10;
 		width = 10;
 	}
-	else if (difficulty == 1)
+	else if (diff == 1)
 	{
 		strDifficulty = "쉬움";
+		height = 3;
+		width = 4;
 	}
-	else if (difficulty == 2)
+	else if (diff == 2)
 	{
 		strDifficulty = "보통";
 		height = 4;
+		width = 4;
 	}
-	else if (difficulty == 3)
+	else if (diff == 3)
 	{
 		strDifficulty = "어려움";
 		height = 4;
@@ -142,18 +139,19 @@ void initDifficulty()
 
 void initAnimalArray()
 {
-	animalArray = (int**)malloc(height);
-	checkAnimal = (int**)malloc(height);
+	animalArray = (int**)malloc(sizeof(int*) * height);
+	checkAnimal = (int**)malloc(sizeof(int*) * height);
 
 	for (int i = 0; i < width; i++)
 	{
-		animalArray[i] = (int*)malloc(width);
-		checkAnimal[i] = (int*)malloc(width);
+		animalArray[i] = (int*)malloc(sizeof(int) * width);
+		checkAnimal[i] = (int*)malloc(sizeof(int) * width);
 	}
 
-	for(int i = 0; i < height; i++)
+	for (int i = 0; i < height; i++)
 	{
-		for (int j = 0; j < width; j++) {
+		for (int j = 0; j < width; j++)
+		{
 			animalArray[i][j] = -1;
 			checkAnimal[i][j] = 0;
 		}
@@ -163,16 +161,21 @@ void initAnimalArray()
 void initAnimalName()
 {
 	int animalSize = (int)(height * width / 2);
-	strAnimal = (int*)malloc(sizeof(int) * animalSize);
+	strAnimal = (const char**)malloc(sizeof(const char*) * animalSize);
 
-	char* animalName[] = {
+	for (int i = 0; i < animalSize; i++)
+	{
+		strAnimal[i] = (const char*)malloc(sizeof(const char) * (animalSize * 2));
+	}
+
+	const char* animalName[] = {
 		"펭귄", "고양이", "미어켓", "캥거루", "거미",
 		"나무늘보", "개구리", "tentacle", "요루", "체임버"
 	};
 
 	for (int i = 0; i < animalSize; i++)
 	{
-		strAnimal[i] = animalName[i];
+		strAnimal[i] = animalName[i % 10];
 	}
 }
 
@@ -233,7 +236,7 @@ void printAnimals()
 
 void printQuestion()
 {
-	printf("\n\n(문제)\n");
+	printf("(문제)\n");
 	int seq = 1;
 
 	for (int i = 0; i < height; i++)
